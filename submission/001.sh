75511f84bab84fa148d24fd9b0b046ea7cbeca28
Caio César <88911909+ckaincode@users.noreply.github.com>
2024-11-24 17:27:58 -0300

    Update 001.sh

--- submission/001.sh
+++ submission/001.sh
@@ -1,5 +1,5 @@
 # What is the hash of block 654,321?
 #!/bin/bash
-hash=$(bitcoin-cli getblockhash 666666 | tr -d "'" | cut -c1-64)
+hash=$(bitcoin-cli getblockhash 654321 | tr -d "'" | cut -c1-64)
 block=$(bitcoin-cli getblock $hash)
 echo $block | jq .nTx
