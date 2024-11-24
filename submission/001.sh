b1a104e9ceb02860cabf0031e64e3f190ddf6e68
Caio César <88911909+ckaincode@users.noreply.github.com>
2024-11-24 16:45:41 -0300

    Update 001.sh

--- submission/001.sh
+++ submission/001.sh
@@ -1,4 +1,4 @@
 # What is the hash of block 654,321?
-hash=$(./bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_056 -rpcpassword=NHgyJIWK6osB getblockhash 654321 | tr -d '\r\n\t' | cut -c1-64)
-block=$(./bitcoin-cli -rpcconnect=84.247.182.145 -rpcuser=user_056 -rpcpassword=NHgyJIWK6osB getblock $hash)
+hash=$(bitcoin-cli getblockhash 654321 | tr -d '\r\n\t' | cut -c1-64)
+block=$(bitcoin-cli getblock $hash)
 echo $block | jq .nTx
